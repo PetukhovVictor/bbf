ea144df9d09c3c26a787967ede80394968e07a48
victor.petukhov <victor.petukhov@jetbrains.com>
2019-11-18 16:05:32 +0300

    1

---
+++ src/test/bbfExecutor/JUnit5ExampleTest.kt
@@ -0,0 +1,12 @@
+package com.stepanov.bbfexecutor
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class JUnit5ExampleTest {
+    @Test
+    fun testSample() {
+        println("1")
+        assertTrue { true }
+    }
+}
